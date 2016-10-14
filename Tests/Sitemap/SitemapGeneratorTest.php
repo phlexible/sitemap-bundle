c1185ec2b99a5690b9e242c81f8c50d05e66659f
Stephan Wentz <swentz@brainbits.net>
2016-10-14 15:36:30 +0200

    change test

--- Tests/Sitemap/SitemapGeneratorTest.php
+++ Tests/Sitemap/SitemapGeneratorTest.php
@@ -14,44 +14,19 @@ use Phlexible\Bundle\SitemapBundle\Event\UrlsetEvent;
 use Phlexible\Bundle\SitemapBundle\Event\XmlSitemapEvent;
 use Phlexible\Bundle\SitemapBundle\Sitemap\SitemapGenerator;
 use Phlexible\Bundle\SitemapBundle\SitemapEvents;
-use Phlexible\Bundle\TreeBundle\ContentTree\ContentTreeInterface;
 use Phlexible\Bundle\TreeBundle\ContentTree\ContentTreeManagerInterface;
 use Phlexible\Bundle\TreeBundle\ContentTree\ContentTreeNode;
 use Phlexible\Bundle\TreeBundle\ContentTree\DelegatingContentTree;
-use Phlexible\Bundle\TreeBundle\Tree\TreeManager;
 use Prophecy\Argument;
-use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Routing\Router;
-use Symfony\Component\Routing\RouterInterface;
 
 class SitemapGeneratorTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var ContentTreeManagerInterface
-     */
-    private $contentTreeManager;
-
-    /**
-     * @var CountryCollection
-     */
-    private $countryCollection;
-
-    /**
-     * @var RouterInterface
-     */
-    private $router;
-
-    /**
-     * @var string
-     */
-    private $siteRootId = '1bcaab4d-098e-4737-ac93-53cae9d83887';
-
     public function testEventDispatcherShouldDispatchUrlGenerationEvent()
     {
-        $contentTreeManager = $this->prophesize(ContentTreeManagerInterface::class);
-        $countryCollection = $this->prophesize(CountryCollection::class);
-        $router = $this->prophesize(Router::class);
+        $siterootId = '1bcaab4d-098e-4737-ac93-53cae9d83887';
+
         $eventDispatcher = $this->prophesize(EventDispatcher::class);
         $eventDispatcher->dispatch(SitemapEvents::URLSET_GENERATION, Argument::type(UrlsetEvent::class))->shouldBeCalled();
         $eventDispatcher->dispatch(SitemapEvents::URL_GENERATION, Argument::type(UrlEvent::class))->shouldBeCalled();
@@ -67,9 +42,14 @@ class SitemapGeneratorTest extends \PHPUnit_Framework_TestCase
         $tree->setLanguage('de')->shouldBeCalled();
         $tree->isPublished($root)->willReturn(true);
 
-        $contentTreeManager->find($this->siteRootId)->willReturn($tree->reveal());
+        $contentTreeManager = $this->prophesize(ContentTreeManagerInterface::class);
+        $contentTreeManager->find($siterootId)->willReturn($tree->reveal());
 
+        $countryCollection = $this->prophesize(CountryCollection::class);
         $countryCollection->filterLanguage('de')->willReturn(array('de'));
+
+        $router = $this->prophesize(Router::class);
+        $router->generate($root, ['_country' => 'de', '_locale' => 'de'], 0)->shouldBeCalled()->willReturn('http://www.test.de');
 
         $sitemap = new SitemapGenerator(
             $contentTreeManager->reveal(),
@@ -79,6 +59,17 @@ class SitemapGeneratorTest extends \PHPUnit_Framework_TestCase
             'de'
         );
 
-        $sitemap->generateSitemap($this->siteRootId);
+        $result = $sitemap->generateSitemap($siterootId);
+
+        $expected = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>http://www.test.de</loc>
+    </url>
+</urlset>
+EOF;
+
+        $this->assertSame($expected, $result);
     }
 }
