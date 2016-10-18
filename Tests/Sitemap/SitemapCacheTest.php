fba615f37f3ada626ed15c3536eb5dca44031d81
Jens Schulze <jdschulze@brainbits.net>
2016-10-18 15:28:41 +0200

    BBPHX-72 vfsStreamWrapper in SitemapCacheTest.php

--- Tests/Sitemap/SitemapCacheTest.php
+++ Tests/Sitemap/SitemapCacheTest.php
@@ -8,18 +8,12 @@
 
 namespace Phlexible\Bundle\SitemapBundle\Tests\Sitemap;
 
-use Phlexible\Bundle\CountryContextBundle\Mapping\CountryCollection;
 use Phlexible\Bundle\SitemapBundle\Sitemap\SitemapCache;
 use Phlexible\Bundle\SitemapBundle\Sitemap\SitemapGenerator;
-use Phlexible\Bundle\SitemapBundle\SitemapEvents;
 use Phlexible\Bundle\SiterootBundle\Entity\Siteroot;
-use Phlexible\Bundle\TreeBundle\ContentTree\ContentTreeManagerInterface;
-use Phlexible\Bundle\TreeBundle\ContentTree\ContentTreeNode;
-use Phlexible\Bundle\TreeBundle\ContentTree\DelegatingContentTree;
 use Prophecy\Argument;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Routing\Router;
+use org\bovigo\vfs\vfsStream;
 
 /**
  * Class SitemapCacheTest
@@ -28,11 +22,15 @@ use Symfony\Component\Routing\Router;
  */
 class SitemapCacheTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetSitemap()
-    {
-        $siterootId = '1bcaab4d-098e-4737-ac93-53cae9d83887';
-        $url = 'http://www.test.de';
-        $xmlSitemap = <<<EOF
+    /**
+     * @var vfsStream
+     */
+    private $cacheRoot;
+
+    /**
+     * @var string
+     */
+    private $xmlSitemap = <<<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
@@ -40,39 +38,47 @@ class SitemapCacheTest extends \PHPUnit_Framework_TestCase
     </url>
 </urlset>
 EOF;
-        $cacheDir = 'fakedir';
 
-//        $tree = $this->prophesize(DelegatingContentTree::class);
-//        $root = new ContentTreeNode();
-//        $root->setId(1);
-//        $root->setTree($tree->reveal());
-//        $tree->getRoot()->willReturn($root);
-//        $tree->hasChildren($root)->willReturn(false);
-//        $tree->get(1)->willReturn($root);
-//        $tree->setLanguage('de')->shouldBeCalled();
-//        $tree->isPublished($root)->willReturn(true);
+    public function setUp()
+    {
+        $this->cacheRoot = vfsStream::setup();
+    }
+
+    public function testGetSitemapForceRebuild()
+    {
+        $siterootId = '1bcaab4d-098e-4737-ac93-53cae9d83887';
 
         $siteRoot = new Siteroot($siterootId);
 
         $sitemapGenerator = $this->prophesize(SitemapGenerator::class);
-        $sitemapGenerator->generateSitemap($siteRoot)->willReturn($xmlSitemap);
+        $sitemapGenerator->generateSitemap($siteRoot)->willReturn($this->xmlSitemap);
 
         $fileSystem = $this->prophesize(Filesystem::class);
         $fileSystem->exists(Argument::any())->willReturn(true);
         $fileSystem->dumpFile(Argument::any(), Argument::any())->willReturn(true);
 
-//        $contentTreeManager = $this->prophesize(ContentTreeManagerInterface::class);
-//        $contentTreeManager->find($siterootId)->willReturn($tree->reveal());
-
-//        $countryCollection = $this->prophesize(CountryCollection::class);
-//        $countryCollection->filterLanguage('de')->willReturn(array('de'));
-
-//        $router = $this->prophesize(Router::class);
-//        $router->generate($root, ['_country' => 'de', '_locale' => 'de'], 0)->shouldBeCalled()->willReturn($url);
-
-        $sitemapCache = new SitemapCache($sitemapGenerator->reveal(), $fileSystem->reveal(), $cacheDir);
+        $sitemapCache = new SitemapCache($sitemapGenerator->reveal(), $fileSystem->reveal(), $this->cacheRoot->url());
         $result = $sitemapCache->getSitemap($siteRoot, true);
 
-        $this->assertSame($xmlSitemap, $result);
+        $this->assertSame($this->xmlSitemap, $result);
+    }
+
+    public function testGetSitemapCached()
+    {
+        $siterootId = '1bcaab4d-098e-4737-ac93-53cae9d83887';
+
+        $siteRoot = new Siteroot($siterootId);
+
+        $sitemapGenerator = $this->prophesize(SitemapGenerator::class);
+        $sitemapGenerator->generateSitemap($siteRoot)->willReturn($this->xmlSitemap);
+
+        $fileSystem = $this->prophesize(Filesystem::class);
+        $fileSystem->exists(Argument::any())->willReturn(true);
+        $fileSystem->dumpFile(Argument::any(), Argument::any())->willReturn(true);
+
+        $sitemapCache = new SitemapCache($sitemapGenerator->reveal(), $fileSystem->reveal(), $this->cacheRoot->url());
+        $result = $sitemapCache->getSitemap($siteRoot, false);
+
+        $this->assertSame($this->xmlSitemap, $result);
     }
 }
