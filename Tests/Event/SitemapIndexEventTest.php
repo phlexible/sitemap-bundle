255420b6efd62ac24c6bd7a1b9ee8908408574ab
Matthias Harmuth <mharmuth@brainbits.net>
2016-12-21 12:07:24 +0100

    BBPHX-100 Add test for sitemapindex event

---
+++ Tests/Event/SitemapIndexEventTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the phlexible sitemap package.
+ *
+ * (c) Stephan Wentz <sw@brainbits.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Phlexible\Bundle\SitemapBundle\Event;
+
+use Phlexible\Bundle\SiterootBundle\Entity\Siteroot;
+use Thepixeldeveloper\Sitemap\SitemapIndex;
+
+/**
+ * SitemapIndex event test
+ *
+ * @author Matthias Harmuth <mharmuth@brainbits.net>
+ *
+ * @covers \Phlexible\Bundle\SitemapBundle\Event\SitemapIndexEvent
+ */
+class SitemapIndexEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSitemapIndexEvent()
+    {
+        $sitemapIndex = new SitemapIndex();
+        $siteroot = new Siteroot('foo');
+
+        $event = new SitemapIndexEvent($sitemapIndex, $siteroot);
+
+        $this->assertSame($sitemapIndex, $event->getSitemapIndex());
+        $this->assertSame($siteroot, $event->getSiteroot());
+    }
+}
